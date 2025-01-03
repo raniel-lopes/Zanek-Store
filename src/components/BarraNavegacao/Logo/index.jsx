a1ee081d3cf0b969af64e2507e2939d2fcaf07fb
Raniel Lopes <raniel.ldms@gmail.com>
2025-01-03 11:14:44 -0300

    atualizando a logo

--- src/components/BarraNavegacao/Logo/index.jsx
+++ src/components/BarraNavegacao/Logo/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import logoMeteora from "/assets/images/logo-meteora.png";
+import logoZanek from "/assets/images/logo-zanek.png";
 
 const Logo = () => {
   return (
     <a className="navbar-brand" href="#">
       <h1 className="m-0">
-        <img className="d-block" src={logoMeteora} alt="Logo da loja Meteora" />
+        <img className="d-block" src={logoZanek} alt="Logo da loja Meteora" />
       </h1>
     </a>
   );
