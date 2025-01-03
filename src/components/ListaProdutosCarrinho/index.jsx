c52029643c98764b130494dd019079a0cc402382
Raniel Lopes <raniel.ldms@gmail.com>
2025-01-03 15:23:59 -0300

    Consumindo o contexto

--- src/components/ListaProdutosCarrinho/index.jsx
+++ src/components/ListaProdutosCarrinho/index.jsx
@@ -3,37 +3,23 @@ import ItemCarrinhoSuspenso from "@/components/CarrinhoSuspenso/ItemCarrinhoSusp
 import ItemCarrinho from "@/components/ItemCarrinho";
 import { useLocation } from "react-router-dom";
 
-const ListaProdutosCarrinho = ({
-  carrinho,
-  adicionarProduto,
-  removerProduto,
-  removerProdutoCarrinho,
-}) => {
+const ListaProdutosCarrinho = ({ carrinho }) => {
   const location = useLocation();
   return (
     <ul className="list-unstyled">
       {carrinho.length === 0 ? (
         <p className="text-center my-5">Não há produtos no carrinho</p>
       ) : (
-        carrinho.map((itemCarrinho) => {
-          return location.pathname === "/carrinho" ? (
-            <ItemCarrinho
-              key={itemCarrinho.id}
-              itemCarrinho={itemCarrinho}
-              adicionarProduto={adicionarProduto}
-              removerProduto={removerProduto}
-              removerProdutoCarrinho={removerProdutoCarrinho}
-            />
+        carrinho.map((itemCarrinho) =>
+          location.pathname === "/carrinho" ? (
+            <ItemCarrinho key={itemCarrinho.id} itemCarrinho={itemCarrinho} />
           ) : (
             <ItemCarrinhoSuspenso
               key={itemCarrinho.id}
               itemCarrinho={itemCarrinho}
-              adicionarProduto={adicionarProduto}
-              removerProduto={removerProduto}
-              removerProdutoCarrinho={removerProdutoCarrinho}
             />
-          );
-        })
+          )
+        )
       )}
     </ul>
   );
