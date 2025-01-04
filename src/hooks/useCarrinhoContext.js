c73f1f7adad63094ba5a48b5728084d906a79c30
Raniel Lopes <raniel.ldms@gmail.com>
2025-01-03 22:29:39 -0300

    Aplicando o useMemo para melhorar a perfomance

--- src/hooks/useCarrinhoContext.js
+++ src/hooks/useCarrinhoContext.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useMemo } from "react"
 import { CarrinhoContext } from "@/context/CarrinhoContext"
 
 export const useCarrinhoContext = () => {
@@ -55,8 +55,8 @@ export const useCarrinhoContext = () => {
         setCarrinho(produto);
     }
 
-    useEffect(() => {
-        const { totalTemp, quantidadeTemp } = carrinho.reduce((acumulador, produto) => ({
+    const { totalTemp, quantidadeTemp } = useMemo(() => {
+        return carrinho.reduce((acumulador, produto) => ({
             quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
             totalTemp: acumulador.totalTemp + produto.preco * produto.quantidade,
         }),
@@ -65,6 +65,12 @@ export const useCarrinhoContext = () => {
                 totalTemp: 0,
             }
         );
+
+    }, [carrinho]);
+
+
+    useEffect(() => {
+
         setQuantidade(quantidadeTemp);
         setValorTotal(totalTemp);
     }, [carrinho])
