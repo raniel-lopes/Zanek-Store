4a40c06090d55ad49f41a74974ecb0006e044c01
Raniel Lopes <raniel.ldms@gmail.com>
2025-01-03 21:08:21 -0300

    Criando o useEffect para calcular o valor total do carrinho

--- src/hooks/useCarrinhoContext.js
+++ src/hooks/useCarrinhoContext.js
@@ -1,8 +1,15 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { CarrinhoContext } from "@/context/CarrinhoContext"
 
 export const useCarrinhoContext = () => {
-    const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+    const {
+        carrinho,
+        setCarrinho,
+        quantidade,
+        setQuantidade,
+        valorTotal,
+        setValorTotal,
+    } = useContext(CarrinhoContext);
 
     function mudarQuantidade(id, quantidade) {
         return carrinho.map((itemDoCarrinho) => {
@@ -47,6 +54,20 @@ export const useCarrinhoContext = () => {
         const produto = carrinho.filter((itemDoCarrinho) => itemDoCarrinho.id !== id);
         setCarrinho(produto);
     }
+
+    useEffect(() => {
+        const { totalTemp, quantidadeTemp } = carrinho.reduce((acumulador, produto) => ({
+            quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
+            totalTemp: acumulador.totalTemp + produto.preco * produto.quantidade,
+        }),
+            {
+                quantidadeTemp: 0,
+                totalTemp: 0,
+            }
+        );
+        setQuantidade(quantidadeTemp);
+        setValorTotal(totalTemp);
+    }, [carrinho])
 
     return {
         carrinho,
