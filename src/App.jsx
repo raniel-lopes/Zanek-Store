b593aab1a417ed995f4c3c12a661930c73be35f4
Raniel Lopes <raniel.ldms@gmail.com>
2025-01-03 23:14:18 -0300

    corrigindo os hooks e aplicando o reducer

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Carrinho from "./pages/Carrinho";
 import PaginaErro from "./pages/PaginaErro";
-import { CarrinhoProdvider } from "./context/CarrinhoContext";
+import { CarrinhoProvider } from "./context/CarrinhoContext";
 
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <CarrinhoProdvider>
+      <CarrinhoProvider>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/carrinho" element={<Carrinho />} />
           <Route path="*" element={<PaginaErro />} />
         </Routes>
-      </CarrinhoProdvider>
+      </CarrinhoProvider>
     </BrowserRouter>
   );
 }
